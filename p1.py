9147ce1fc4ffeb23baeabe3ea00155a56e1bffa9
shouvikantu <shouvikwu26@gmail.com>
2024-12-02 19:50:55 -0800

    Video 3

--- p1.py
+++ p1.py
@@ -2,6 +2,7 @@ inputs = [1,2,3, 2.5]
 weights1 = [0.2, 0.8, -0.5, 1.0]
 weights2 = [0.5, -0.91, 0.26, -0.5]
 weights3 = [-0.26, -0.27, 0.17, 0.87]
+
 bias1 = 2
 bias2 = 3
 bias3 = 0.5
@@ -11,3 +12,27 @@ output = [inputs[0]* weights1[0] + inputs[1]* weights1[1] +inputs[2]* weights1[2
           inputs[0]* weights3[0] + inputs[1]* weights3[1] +inputs[2]* weights3[2] +inputs[3]* weights3[3] +bias3]
 
 print(output)
+
+## Transferring to np and new code, same problem
+import numpy as np
+
+# For singular neuron
+
+inputs = [1,2,3,2.5]
+weights = [0.2, 0.8, -0.5, 1.0]
+bias = 2
+
+output = np.dot(weights , inputs) +bias
+print(output)
+
+
+# For all layers
+inputs = [1,2,3, 2.5]
+
+weights=[[0.2, 0.8, -0.5, 1.0],[0.5, -0.91, 0.26, -0.5],[-0.26, -0.27, 0.17, 0.87]]
+
+biases = [2,3,0.5]
+
+output = np.dot(weights, inputs) +biases
+print(output)
+
