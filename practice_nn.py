018c733bff004adeadc1dc98867e40aefa173423
shouvikantu <shouvikwu26@gmail.com>
2024-12-03 16:15:20 -0800

    Practiced everything from videos 1-4

--- practice_nn.py
+++ practice_nn.py
@@ -29,3 +29,23 @@ print(input_layer1)
 input_layer2 = np.dot(input_layer1, np.array(weights2).T) + biases2
 print(input_layer2)
 
+## Let's make classes
+
+X = [[1,2,3,4,5], [2.1, 3, -2.1, 2.2, 3], [1.2, 2.1, 3.0, 2.2, 1.2]]
+
+class Layer_dense:
+    def __init__(self, n_inputs, n_neurons) -> None:
+        self.weights = np.random.randn(n_inputs, n_neurons)
+        self.bias = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + bias
+
+
+layer1 = Layer_dense(5,10)
+layer2 = Layer_dense(10,2)
+
+layer1.forward(X)
+print(layer1.output)
+
+layer2.forward(layer1.output)
+print(layer2.output)
