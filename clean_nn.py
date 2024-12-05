e0e084c2289595921992ad8108b5a29a7d545ac9
shouvikantu <shouvikwu26@gmail.com>
2024-12-05 14:35:18 -0800

    Softmax activation implemented.

--- clean_nn.py
+++ clean_nn.py
@@ -5,7 +5,7 @@ from nnfs.datasets import spiral_data
 
 #X = [[1,2,3,2.5], [2.0, 5.0, -1.0, 2.0], [-1.5, 2.7, 3.3, -0.8]]
 
-X, y = spiral_data(100,3)
+X, y = spiral_data(samples=100, classes=3)
 
 
 
@@ -21,11 +21,24 @@ class Activation_ReLu:
     def forward(self, inputs):
         self.output = np.maximum(0,inputs)
 
+class Activation_Softmax():
+    def forward(self, inputs):
+        exp_values =  np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output= probabilities
 
 
-layer1= Dense_Layer(2, 5)
+dense1= Dense_Layer(2, 3)
 activation1 = Activation_ReLu()
 
-layer1.forward(X)
-activation1.forward(layer1.output)
-print(activation1.output)
+dense2 = Dense_Layer(3,3)
+activation2 = Activation_Softmax()
+
+
+dense1.forward(X)
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+print(activation2.output[:5])
