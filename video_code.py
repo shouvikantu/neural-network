dd240fe76c7033b838983ed2a1bc66adce359dcf
shouvikantu <shouvikwu26@gmail.com>
2024-12-05 14:59:14 -0800

    Categorical Cross Entropy

--- video_code.py
+++ video_code.py
@@ -165,3 +165,14 @@ exp_values = np.exp(layer_outputs)
 norm_values = exp_values / np.sum(exp_values, axis=1, keepdims=True)
 
 print(norm_values)
+
+
+## Categorical Cross Entropy
+import math
+
+softmax_ouput = [0.7, 0.1, 0.2]
+target_output=[1,0,0]
+
+loss = -(math.log(softmax_ouput[0]*target_output[0]+softmax_ouput[1]*target_output[1]+softmax_ouput[2]*target_output[2]))
+
+print(loss)
